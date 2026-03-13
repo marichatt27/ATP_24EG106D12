99080dced981c39a036582b3739e2a47f3c9b523
marichatt27 <srutakeerthivarkala@gmail.com>
2026-03-13 21:29:54 +0530

    Delete spread_restParameter directory

--- spread_restParameter/rest_parameter.js
+++
@@ -1,9 +0,0 @@
-// 💡 Exercise 3: Create a function that receives any number of
-//  args as arguments and return their sum using REST parameter
-
-arr=[10,20,30,40,50,60,70]
-let [a,b,c,d]=arr
-let sum=a+b+c+d
-console.log(sum)
-
-    
--- spread_restParameter/spread_parameter.js
+++
@@ -1,9 +0,0 @@
-//write a function that receives any number of arg and returns sum
-function sum(...a){
-    // let sum=0     
-    // for(i of a)
-    // sum=sum+i
-    // return sum    
-    return a.reduce((acc,ele)=>acc+ele)
-}
-console.log(sum(10,20,30,40,50))
